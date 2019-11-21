7d5918c82579de220cab7fc4076917d328375a34
Nico Virgino <nicovirgino@gmail.com>
2019-11-21 14:11:52 -0600

    finished million for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,15 +4,23 @@ public class ControlFlowExercises {
 //        while (i <= 15){
 //            System.out.println(i++);
 //        }
+
 //
 //        long i = 2L;
 //        do{
 //            System.out.println(i);
 //            i = i*i;
 //        }while (i <= 1000000);
-        for(int i = 5; i <= 15; i++){
-            System.out.println(i++);
-        }
+
+
+//        for(int i = 5; i <= 15; i++){
+//            System.out.println(i++);
+//        }
+
+//        for(long i = 2L; i <= 1000000; i*=i){
+//            System.out.println(i);
+//        }
+
     }
 
 }
