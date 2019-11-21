8a4533a7da6500cf5672dc6cc84a923e26a252fd
Nico Virgino <nicovirgino@gmail.com>
2019-11-21 08:41:11 -0600

    hello world exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,69 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+
+
+
+        //create an int variable named myFavoriteNumber and assign your
+        // favorite number to it, then print it out to the console.
+        int myFavoriteNumber = 23;
+        System.out.println(myFavoriteNumber);
+
+        //Create a String variable named myString and assign a string
+        // value to it, then print the variable out to the console.
+        String myString = "Banana";
+        System.out.println(myString);
+
+        //Change your code to assign a character value to myString.
+        // What do you notice?
+        myString = "OtherBanana";
+        System.out.println(myString);
+
+        //Change your code to assign the value 3.14159 to myString.
+        //What happens?
+//        myString = 3.14159
+
+        //Declare an long variable named myNumber, but do not assign anything to it.
+        // Next try to print out myNumber to the console.
+        // What happens?
+        long myNumber;
+
+
+        //Change your code to assign the value 3.14 to myNumber.
+        // What do you notice?
+//        myNumber = 3.14;
+
+
+        //Change your code to assign the value 123L (Note the 'L' at the end)
+        // to myNumber.
+        myNumber= 123L;
+
+        //Change your code to assign the value 123 to myNumber
+
+        myNumber = 123;
+        System.out.println(myNumber);
+        //Change your code to declare myNumber as a float. Assign the value 3.14 to it.
+        // What happens? What are two ways we could fix this?
+//       float myNumber = 3.14;
+
+       //Copy and paste the following code blocks one at a time and execute them
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        //Try to create a variable named class.
+        //What happens?
+//        int class
+
+        //Object is the most generic type in Java. You can assign any value to a variable of type Object.
+        //What do you think will happen when the following code is run?
+        Object hello;
+        System.out.println();
+
     }
 }
 
