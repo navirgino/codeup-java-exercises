22f71de2ad1d98b5628533f612bcf0181537d38a
Nico Virgino <nicovirgino@gmail.com>
2019-11-21 10:55:00 -0600

    console exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,60 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//
+//        System.out.format("Pi: %.2f " + pi);
+
+        Scanner scanner = new Scanner(System.in);
+       scanner.useDelimiter("\n");
+
+
+        System.out.print("Enter Three Words (with commas): ");
+        String firstInput = scanner.next();
+        String secondInput = scanner.next();
+        String thirdInput = scanner.next();
+        System.out.printf("%s%n", firstInput);
+        System.out.printf("%s%n", secondInput);
+        System.out.printf("%s%n", thirdInput);
+
+
+
+        System.out.println("You entered: --> \"" + firstInput + " " + secondInput + " " + thirdInput + "\" <--");
+
+        scanner.nextLine();
+
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+
+        System.out.println(sentence);
+
+        scanner.nextLine();
+
+        System.out.print("Enter length of classroom: ");
+        int length = scanner.nextInt();
+//        int numLength = Integer.parseInt(length);
+
+
+        System.out.print("Enter width of classroom: ");
+        int width = scanner.nextInt();
+//        int numWidth = Integer.parseInt(width);
+
+        System.out.println("Enter a height: ");
+        float height = scanner.nextFloat();
+//        float numHeight = Float.parseFloat(height);
+
+        System.out.println("Area of the classroom is: " + length* width);
+
+
+        System.out.println("Perimeter of the classroom is " + ((length*2) + (width*2)));
+
+
+        System.out.println("Volume: " + (height * length * width) );
+
+
+    }
+
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -61,9 +61,27 @@ public class HelloWorld {
 
         //Object is the most generic type in Java. You can assign any value to a variable of type Object.
         //What do you think will happen when the following code is run?
-        Object hello;
-        System.out.println();
+//        Object hello;
+//        System.out.println();
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//        x = x + 5;
+//
+//
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
 
     }
 }
+
+
 
