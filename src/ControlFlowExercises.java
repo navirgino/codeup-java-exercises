54b4a16c9ba3b692c00607d78b18821c9e40115a
Nico Virgino <nicovirgino@gmail.com>
2019-11-21 14:14:44 -0600

    finished fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -21,6 +21,18 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
+        for(int i = 1; i <= 100; i++){
+            if(i%15 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i%3 == 0){
+                System.out.println("Fizz");
+            }else if(i%5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+
     }
 
 }
