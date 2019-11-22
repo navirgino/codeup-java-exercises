b9844178f06c086dd6bfbd3f96ae587d2567549b
Nico Virgino <nicovirgino@gmail.com>
2019-11-22 15:06:56 -0600

    method lecture

---
+++ src/MethodsExercises.java
@@ -0,0 +1,11 @@
+public class MethodsExercises {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+        public static void main (String[]args){
+            System.out.println(add(3, 4));
+
+        }
+
+}
