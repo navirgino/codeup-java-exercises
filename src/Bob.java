19c99e6752405fecf73817964037f829c1bd580e
Nico Virgino <nicovirgino@gmail.com>
2019-11-22 10:28:48 -0600

    bob time

---
+++ src/Bob.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useDelimiter(("\n"));
+
+        System.out.println("Tell Bob something: ");
+
+        String input = sc.next();
+
+        do {
+
+            if (input.endsWith("?")) {
+                System.out.println("Bob: Sure");
+            }
+            else if (input.endsWith("!")) {
+                System.out.println("Bob: Whoa chill out!");
+            }
+            else if (input.equals("")) {
+                System.out.println("Bob: Fine, be that way!!!");
+            }
+            else {
+                System.out.println("Whatever");
+            }
+            input = sc.next();
+
+        } while (!input.equals("I'm done"));
+
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in).useDelimiter(("\n"));
+
+        System.out.println("We don't need no education");
+        System.out.println("We don't need no thought control");
+        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+        System.out.println("In windows, the main drive is usually C:\\");
+        System.out.println("I can do backslashes \\, double backslashes \\\\,");
+        System.out.println("and the amazing triple backslash \\\\\\!");
+
+
+        //Create a class named Bob with a main method for the following exercise.
+        //Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+
+
+    }
+}
+
+
