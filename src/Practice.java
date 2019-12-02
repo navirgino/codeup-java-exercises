8df12676cc79b8b37302a2e3b836effdcee68bdd
Nico Virgino <nicovirgino@gmail.com>
2019-12-01 21:13:13 -0600

    practicing java

---
+++ src/Practice.java
@@ -0,0 +1,84 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        //int can only store whole numbers [1, 2, 3, 4, . . .] (up to 2bill)
+        //byte range = [-127, 127]; 1 mem
+//        byte age = 30;
+//        int viewsCount = 2_123_456_789;
+//        //need suffix to represent number as a long
+//        long longCount = 3_123_456_789L;
+//        //need suffix to represent number as a float
+//        float price = 10.99f;
+//        char letter = 'A';
+//        boolean isEligible = false;
+////
+//        byte age = 30;
+//        Date now = new Date();
+//        System.out.println(now);
+//        byte x = 1;
+//        byte y = x;
+//        x = 2;
+//        //y is not affected because they are different from each other
+//        System.out.println(y);
+//        Point point1 = new Point(1, 1);
+//        Point point2 = point1;
+//        point1.x = 2;
+//
+//        System.out.println(point1);
+//        System.out.println(point2);
+
+//        String message = "   ello" + " wurld!!";
+////        message.endsWith("!!");
+//        System.out.println(message.endsWith("!!"));
+//        System.out.println(message.length());
+//        System.out.println(message.indexOf("!"));
+//        System.out.println(message.replace("!", "*"));
+//        System.out.println(message);
+//        System.out.println(message.trim());
+//        String message = "c:\tWindows\\...";
+//        System.out.println(message);
+
+        //arrays down below
+//        int[] numbers = new int[5];
+////       old syntax for array down below
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+//        System.out.println(Arrays.toString(numbers));
+//        System.out.println(numbers);
+
+//        //new syntax for array down below (arrays are fixed length)
+//        int[] numbers = {2, 3, 4, 5, 1, 4};
+//        Arrays.sort(numbers);
+//        System.out.println(numbers.length);
+//        System.out.println(Arrays.toString(numbers));
+
+////multi-dimensional array first brace are how many rows, second how many columns.
+//        int[][] numbers = new int[2][3];
+//        //new way:
+//        int[][] numbers = {{1, 2, 3}, {4, 5, 6}};
+//        numbers[0][0] = 1;
+//        System.out.println(Arrays.deepToString(numbers));
+
+
+        //final means that java treats it as a constant
+//        final float pi = 3.14F;
+        ////////////////////////////////
+
+//        //implicit casting
+//        // byte > short > int > long > float > double
+//        String x = "1";
+//        int y = Integer.parseInt(x) + 2; //2.0 + 1.1
+//        double z = Double.parseDouble(x) + 4;
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(z);
+
+
+
+
+    }
+}
