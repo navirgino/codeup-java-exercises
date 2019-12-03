58ac04506a8ff96b29c7ddd48604e48819b273c3
Nico Virgino <nicovirgino@gmail.com>
2019-12-03 15:13:02 -0600

    some extra freshening up

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,23 +1,22 @@
 package shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
+    protected double length;
+    protected double width;
 
     //constructor for rectangle
-    public Rectangle(int lengthParam, int widthParam) {
+    public Rectangle(double lengthParam, double widthParam) {
         this.length = lengthParam;
         this.width = widthParam;
     }
 
-    //constructor for area
-    public int getArea() {
-        return length * width;
-    }
-
     //constructor for perimeter
-    public int getPerimeter() {
-        return (2 * length) + (2 * width);
+    public double getPerimeter() {
+        return (2 * this.length) + (2 * this.width);
+    }
+    //constructor for area
+    public double getArea() {
+        return this.length * this.width;
     }
 
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,27 +2,34 @@ package shapes;
 
 public class Square extends Rectangle {
 
-    int side;
+//    double side;
 
     //define a constructor that accepts on arg - side
-    public Square(int side){
+    public Square(double side){
+
         //call the parent's constructor
         super(side, side);
-        this.side = side;
+
         //set both the length and width to the val of side
+//        this.side = side;
+
     }
 
 
-    @Override
-    public int getArea(){
-//        System.out.println(4 * side);
-        return 4 * side;
-    }
+
 
     @Override
-    public int getPerimeter(){
+    public double getPerimeter(){
 //        System.out.println(side * side);
-        return side * side;
+//        return side * side;
+        return 4 * super.width;
+    }
+
+    @Override
+    public double getArea(){
+//        System.out.println(4 * side);
+//        return 4 * side;
+        return Math.pow(super.length, 2);
     }
 
 }
