3d46c7cc47d09057187d42ab98e465453741480e
Nico Virgino <nicovirgino@gmail.com>
2019-12-08 18:21:02 -0600

    studying stuff

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,12 +1,12 @@
 package util;
 
-
-
 import java.util.Scanner;
 
-public class Input {
+
+public class Input extends Exception {
 
 //    private String scanner;
+
 
     private Scanner scanner = new Scanner(System.in);
 
@@ -17,51 +17,66 @@ public class Input {
     public boolean yesNo() {
 //        System.out.println("Type yes/no");
         String response = scanner.nextLine();
-        if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yeet")){
+        if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yeet")) {
             return true;
-        }else {
+        } else {
             return false;
         }
-
     }
-
 
     public int getInt(int min, int max) {
         System.out.println("Enter a number between 1 and 10: ");
-        int input = Integer.parseInt(scanner.nextLine());
-        if (input < min) {
-            System.out.println("The number is lower than the min, try again: ");
-            return getInt(min, max);
-        } else if (input > max) {
-            System.out.println("The number is higher than the max, try again: ");
-            return getInt(min, max);
+        int input = Integer.valueOf(getString());
+
+        try {
+            if (input <= min) {
+                System.out.println("The number is lower than the min, try again: ");
+                return getInt(min, max);
+            } else if (input >= max) {
+                System.out.println("The number is higher than the max, try again: ");
+                return getInt(min, max);
+            }
+//            return input;
+
+        } catch (Exception e) {
+            getInt(min, max);
+            System.out.println("ok?");
+
         }
+        return input;
+    }
+
+
+    public double getDouble(double min, double max) {
+        System.out.println("Enter a number to double between 1.0 and 10.0: ");
+        double input = Double.valueOf(getString());
+        try {
+
+            if (input <= min) {
+                System.out.println("The number is lower than the min, try again: ");
+                return getDouble(min, max);
+            } else if (input >= max) {
+                System.out.println("The number is higher than the max, try again: ");
+                return getDouble(min, max);
+            }
+                return input;
+
+        } catch (Exception e) {
+            getDouble(min, max);
+        }
+        return input;
+    }
+
+
+    public double getDouble() {
+        System.out.println("Input a number");
+        double input = scanner.nextDouble();
+        System.out.println("Your input was " + input);
         return input;
     }
 
     public int getInt() {
         System.out.println("Input a number: ");
-        int input = scanner.nextInt();
-        System.out.println("Your input was " + input);
-        return input;
-
-    }
-
-    public double getDouble(double min, double max) {
-        System.out.println("Enter a number to double between 1.0 and 10.0: ");
-        double input = Integer.parseInt(scanner.nextLine());
-        if (input < min) {
-            System.out.println("The number is lower than the min, try again: ");
-            return getDouble(min, max);
-        } else if (input > max) {
-            System.out.println("The number is higher than the max, try again: ");
-            return getDouble(min, max);
-        }
-        return input;
-    }
-
-    public double getDouble() {
-        System.out.println("Input a number");
         int input = scanner.nextInt();
         System.out.println("Your input was " + input);
         return input;
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,15 +3,17 @@ package util;
 public class InputTest {
 
     public static void main(String[] args) {
-//        Input input = new Input();
-//        input.getInt(1, 10);
-//        input.getInt();
+        Input input = new Input();
+        System.out.println("Enter [y/n]");
+        System.out.println(input.yesNo());
+        input.getInt(1, 10);
+        input.getInt();
 
-//        System.out.println(input.yesNo());
+        System.out.println(input.yesNo());
 
-//        Input newInput = new Input();
-//        newInput.getDouble(1, 10);
-//        newInput.getDouble();
+        Input newInput = new Input();
+        newInput.getDouble(1, 10);
+        newInput.getDouble();
 
 
     }
