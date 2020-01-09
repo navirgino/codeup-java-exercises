5a7f5b72a3e02dac97045b57237cef5443eaf4da
Nico Virgino <nicovirgino@gmail.com>
2020-01-09 10:06:15 -0600

    morning java warmups

---
+++ src/study/WarmingUp.java
@@ -0,0 +1,12 @@
+package study;
+
+public class WarmingUp {
+    public static int SimpleAdding(int num){
+        int bucket = 0;
+        for(int i = 1; i <= num; i++){
+            bucket += i;
+        }
+        return bucket;
+    }
+
+}
